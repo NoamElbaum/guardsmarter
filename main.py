30114433a1f6e5aba98983b4264a92ec410aaeaf
NoamElbaum <noam272000@gmail.com>
2020-04-01 15:53:23 +0300

    added interrupt

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import plate
 import face_rec
+import UART
 import Bot
-
 
 num = plate.read_plate('plate2.jpg')
 name = face_rec.classify_face('test3.jpg')
 print(f'plate: {num}\nname: {name}')
 while 1:
-    pass
+    if UART.interrupt_check():
+        pass  # call for help
