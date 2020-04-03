179a8a18c8830e0bf3d11b221a5cd063d1dd7a4f
shon12bar <shon_bar1@walla.com>
2020-04-03 18:44:31 +0300

    added Bot system

--- Bot.py
+++ Bot.py
@@ -1,9 +1,22 @@
 from win32com.client import Dispatch
-
+import speech_recognition as sr
 
 def Robo(text):
     robo = Dispatch("SAPI.SpVoice")
     robo.Speak(text)
+
+
+def Listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Hello, please say your name :")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio,  language='he')
+            return text
+        except:
+            print("Sorry could not recognize what you said")
+
 
 
 Robo("Hello")
