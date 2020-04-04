779ee3c5a98cf148f111230f12df8ddf647cea0d
NoamElbaum <noam272000@gmail.com>
2020-04-04 17:33:14 +0300

    bot update
    marged speak and listen functions and switched to gtts module for text to speech

--- Bot.py
+++ Bot.py
@@ -1,9 +1,29 @@
-from win32com.client import Dispatch
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+import os
 
 
-def Robo(text):
-    robo = Dispatch("SAPI.SpVoice")
-    robo.Speak(text)
+def speak(text):
+    out = gTTS(text=text, lang='en', slow=False)
+    out.save('play.mp3')
+    playsound("play.mp3")
+    os.remove("play.mp3")
 
 
-Robo("Hello")
+def Listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language='he')
+            return text
+        except:
+            print("Sorry could not recognize what you said")
+
+
+if __name__ == '__main__':
+    speak('hello what is your name')
+    print('name: ')
+    text = Listen()
+    print(text)
