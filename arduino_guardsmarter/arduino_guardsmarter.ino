5a90b362c7c5207e32e76b641c028e11c5280b10
NoamElbaum <noam272000@gmail.com>
2020-04-10 13:41:19 +0300

    added RFID reader

--- arduino_guardsmarter/arduino_guardsmarter.ino
+++ arduino_guardsmarter/arduino_guardsmarter.ino
@@ -2,9 +2,12 @@
 #include <MFRC522.h>
 
 #define gate_servo 6
-#define intPin 2
+#define intPin     2
+#define RST_PIN    9           // Configurable, see typical pin layout above
+#define SS_PIN     10          // Configurable, see typical pin layout above
 
 Servo gate;
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 char command = 'c';
 int gate_pos = 90;
@@ -12,10 +15,13 @@ int gate_pos = 90;
 void setup() 
 {
   Serial.begin(9600);
+  SPI.begin();                                           // Init SPI bus
+  mfrc522.PCD_Init();                                    // Init MFRC522 card
+  
   gate.attach(gate_servo);
   pinMode(13,OUTPUT);
   digitalWrite(13,LOW);
-  attachInterrupt(digitalPinToInterrupt(intPin), help, RISING)
+  attachInterrupt(digitalPinToInterrupt(intPin), help, RISING);
 }
 
 void loop() 
@@ -36,15 +42,28 @@ void loop()
    digitalWrite(13,HIGH);
    gate_pos = 180; 
   }
+  else if(command == 'r')
+    RFID_read();
 }
 
 void help()
 {
   while(!Serial.available())
   {
-    Serial.ptintln('h');
+    Serial.println('h');
   }
   char val = Serial.read();
   if(val != 'g')
     help();
+}
+
+void RFID_read() 
+{  
+  if ( ! mfrc522.PICC_IsNewCardPresent()) // Reset the func if no new card present on the sensor/reader.
+    RFID_read();
+   
+  if ( ! mfrc522.PICC_ReadCardSerial())   // Select one of the cards
+    RFID_read();
+
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid)); //send the info from the card to the Serial bus
 }
