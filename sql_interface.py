c4a3855b1ba5f6330e00ceb1b37189e984266c7f
NoamElbaum <noam272000@gmail.com>
2020-04-13 18:42:56 +0300

    orgenized the code

--- sql_interface.py
+++ sql_interface.py
@@ -7,19 +7,7 @@ mydb = mysql.connector.connect(
     database="guardsmarter",
     use_pure=True
 )
-
 DB = mydb.cursor()
-
-
-def add_resident(ID, carNum, picPath, f_name, l_name):
-    with open(picPath, 'rb') as pic:
-        face = pic.read()
-    print(face)
-    query = """insert into residents values(%s, %s,%s,%s,%s)"""
-    data = (ID, carNum, face, f_name, l_name)
-    DB.execute(query, data)
-
-    mydb.commit()
 
 
 def read(select_frase):
@@ -42,15 +30,27 @@ def write(query, data):
     mydb.commit()
 
 
-if __name__ == '__main__':
-    # id_num = int(input('enter residents id: '))
-    # car_num = int(input('enter car num: '))
-    # img_path = str(input('enter pic path: '))
-    # f_name = str(input('enter first name: '))
-    # l_name = str(input('enter last name: '))
-    # add_resident(id_num , car_num, img_path, f_name, l_name)
+def add_resident(ID, carNum, picPath, f_name, l_name):
+    with open(picPath, 'rb') as pic:
+        face = pic.read()
+    print(face)
+    query = """insert into residents values(%s, %s,%s,%s,%s)"""
+    data = (ID, carNum, face, f_name, l_name)
+    DB.execute(query, data)
+    mydb.commit()
 
-    # run only once.
-    add_resident(211715966, 1234590, 'faces/NoamElbaum.jpg', 'נועם', 'אלבאום')
-    add_resident(213546879, 2354430, 'faces/donald trump.jpg', 'דונלד','טראמפ')
-    add_resident(322759226, 83494801, 'faces/Shon Bar.jpg', 'דונלד', 'טראמפ')
+
+if __name__ == '__main__':
+
+    id_num = int(input('enter residents id: '))
+    car_num = int(input('enter car num: '))
+    img_path = str(input('enter pic path: '))
+    f_name = str(input('enter first name: '))
+    l_name = str(input('enter last name: '))
+    add_resident(id_num , car_num, img_path, f_name, l_name)
+
+    # run only once to add three example residents.
+    # add_resident(211715966, 1234590, 'faces/NoamElbaum.jpg', 'נועם', 'אלבאום')
+    # add_resident(123456789, 62510701, 'faces/bibi.jpg', 'בינימין', 'נתניהו')
+    # add_resident(987654321, 83494801, 'faces/donald trump.jpg', 'דונלד', 'טראמפ')
+
