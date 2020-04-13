feef69fd381dcee86dd8e7df2482af193579d97d
NoamElbaum <noam272000@gmail.com>
2020-04-13 18:43:32 +0300

    packed connction code in a func

--- terminal_network.py
+++ terminal_network.py
@@ -1,22 +1,25 @@
 import socket
 
+client = socket.socket()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
 
-print('waiting for connection')
-client, address = s.accept()
-print(f'connected to {client} {address}')
-
+def connect():
+    global client
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), 1234))
+    s.listen(1)
+    print('waiting for connection')
+    client, address = s.accept()
+    print(f'connected to {client}\n{address}')
 
 
 def alert():
-    client.send(bytes("come to the gate", "utf-8"))
+    client.send(bytes("הגיעה התראה משער היישוב נא להגיע במהירות", "utf-8"))
 
 
 if __name__ == '__main__':
+    connect()
     while 1:
-        act = input('to allert send a: ')
+        act = input('to alert send a: ')
         if act == 'a':
             alert()
